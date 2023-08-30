0add119a3701a57886d74c07199587afd9854ced
ekkamon <ekkb.contact@gmail.com>
2023-08-31 00:04:32 +0700

    update!: add logic click pop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,47 @@
-import { DataInterface } from "@/interfaces/DataInterface";
 import pb from "@/services/pocketbase";
 
+import { useEffect, useRef, useState } from "react";
+import type { DataInterface } from "@/interfaces/DataInterface";
+
+// rate update amount to database
+const sendPerCount = 7;
+
 export default function Home() {
-  const update = async () => {
-    await pb.collection("data").update<DataInterface>("vu7tba7w24ye4rq", {
-      "count+": 1,
-    });
+  const stash = useRef<number>(0);
+  const [score, setScore] = useState<number>(0);
+
+  const handleClick: () => Promise<void> = async () => {
+    setScore((p) => p + 1);
+    stash.current += 1;
+
+    if (stash.current == sendPerCount) {
+      stash.current = 0;
+      try {
+        console.log("send", score);
+        await pb.collection("data").update<DataInterface>("vu7tba7w24ye4rq", {
+          "count+": 5,
+        });
+      } catch {}
+    }
   };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+
+    return () => document.removeEventListener("click", handleClick);
+  }, []);
 
   return (
     <>
-      <div onClick={update}>pop</div>
+      <div
+        key={score}
+        className="flex justify-center py-[1rem] text-[2.5rem]"
+        style={{
+          animation: "shaking 0.25s",
+        }}
+      >
+        {score.toLocaleString()}
+      </div>
     </>
   );
 }
