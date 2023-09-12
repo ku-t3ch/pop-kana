025e2f823fb039cb0a85c9bc20757068afdbb6ac
Teerut Srithongdee <teerutsr@gmail.com>
2023-09-12 12:42:00 +0700

    optimize bot protect

--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -1,24 +1,20 @@
 //type
 import type { NextApiRequest, NextApiResponse } from "next";
-import type { DataInterface } from "@/interfaces/DataInterface";
+import requestIp from "request-ip";
 
+import type { DataInterface } from "@/interfaces/DataInterface";
 import pb from "@/services/pocketbase";
 import { env } from "@/env.mjs";
 import rateLimit from "@/utils/rate-limit";
 
 const limiter = rateLimit({
   interval: 10 * 1000, // 60 seconds
-  uniqueTokenPerInterval: 500, // Max 500 users per second
+  uniqueTokenPerInterval: 1000, // Max 500 users per second
 });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { body, method } = req;
-
-  try {
-    await limiter.check(res, 2, "CACHE_TOKEN");
-  } catch (error) {
-    return res.status(429).json({ message: "Too Many Requests" });
-  }
+  const detectedIp = requestIp.getClientIp(req);
 
   if (method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
@@ -28,6 +24,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   if (count >= 100 || count < 0) {
     return res.status(400).json({ message: "hmmmm!" });
+  }
+
+  try {
+    if (count >= 80 && count <= 99) {
+    //   console.log(`${detectedIp}-CACHE_TOKEN_1`);
+      await limiter.check(res, 2, `${detectedIp}-CACHE_TOKEN_1`);
+    } else if (count >= 60 && count < 79) {
+    //   console.log(`${detectedIp}-CACHE_TOKEN_2`);
+      await limiter.check(res, 3, `${detectedIp}-CACHE_TOKEN_2`);
+    } else if (count >= 40 && count < 59) {
+    //   console.log(`${detectedIp}-CACHE_TOKEN_3`);
+      await limiter.check(res, 4, `${detectedIp}-CACHE_TOKEN_3`);
+    } else if (count >= 20 && count < 39) {
+    //   console.log(`${detectedIp}-CACHE_TOKEN_4`);
+      await limiter.check(res, 5, `${detectedIp}-CACHE_TOKEN_4`);
+    } else if (count >= 0 && count < 19) {
+    //   console.log(`${detectedIp}-CACHE_TOKEN_5`);
+      await limiter.check(res, 5, `${detectedIp}-CACHE_TOKEN_5`);
+    }
+  } catch (error) {
+    return res.status(429).json({ message: "Too Many Requests" });
   }
 
   try {
